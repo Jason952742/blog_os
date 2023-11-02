f8cd235cd1f55f4163f6c47f9bd3ffd1f5f00d45
jason <jason@jaasun.com>
2023-11-02 21:55:32 +0800

    Double Faults

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,19 @@ pub extern "C" fn _start() -> ! {
     blog_os::init();
 
     // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3(); // new
+    x86_64::instructions::interrupts::int3();
+
+    // trigger a page fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
+
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
